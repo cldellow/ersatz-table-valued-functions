98f6c4ed7d60bc8d2fd41ac9cfe09559a6ebc012
Colin Dellow <cldellow@gmail.com>
2023-01-20 20:40:39 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# ersatz-table-valued-functions
+
+[![PyPI](https://img.shields.io/pypi/v/ersatz-table-valued-functions.svg)](https://pypi.org/project/ersatz-table-valued-functions/)
+[![Changelog](https://img.shields.io/github/v/release/cldellow/ersatz-table-valued-functions?include_prereleases&label=changelog)](https://github.com/cldellow/ersatz-table-valued-functions/releases)
+[![License](https://img.shields.io/badge/license-Apache%202.0-blue.svg)](https://github.com/cldellow/ersatz-table-valued-functions/blob/main/LICENSE)
+
+A bad idea.
+
+## Installation
+
+Install this library using `pip`:
+
+    pip install ersatz-table-valued-functions
+
+## Usage
+
+Usage instructions go here.
+
+## Development
+
+To contribute to this library, first checkout the code. Then create a new virtual environment:
+
+    cd ersatz-table-valued-functions
+    python -m venv venv
+    source venv/bin/activate
+
+Now install the dependencies and test dependencies:
+
+    pip install -e '.[test]'
+
+To run the tests:
+
+    pytest
---
+++ setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup
+import os
+
+VERSION = "0.1"
+
+
+def get_long_description():
+    with open(
+        os.path.join(os.path.dirname(os.path.abspath(__file__)), "README.md"),
+        encoding="utf8",
+    ) as fp:
+        return fp.read()
+
+
+setup(
+    name="ersatz-table-valued-functions",
+    description="A bad idea.",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    author="Colin Dellow",
+    url="https://github.com/cldellow/ersatz-table-valued-functions",
+    project_urls={
+        "Issues": "https://github.com/cldellow/ersatz-table-valued-functions/issues",
+        "CI": "https://github.com/cldellow/ersatz-table-valued-functions/actions",
+        "Changelog": "https://github.com/cldellow/ersatz-table-valued-functions/releases",
+    },
+    license="Apache License, Version 2.0",
+    version=VERSION,
+    packages=["ersatz_table_valued_functions"],
+    install_requires=["sqlglot"],
+    extras_require={"test": ["pytest", "pytest-watch"]},
+    python_requires=">=3.7",
+)
